5ac88b2ac3f97f52c6411867ecaa84220ebcfe64
Francisco Gorina Vanrell <fgorina@gmail.com>
2023-04-24 16:04:00 +0200

    Initial Commit

---
+++ S57Browser/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  S57Browser
+//
+//  Created by Francisco Gorina Vanrell on 24/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ S57Browser/S57BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  S57BrowserApp.swift
+//  S57Browser
+//
+//  Created by Francisco Gorina Vanrell on 24/4/23.
+//
+
+import SwiftUI
+
+@main
+struct S57BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
